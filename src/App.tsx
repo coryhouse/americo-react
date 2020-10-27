c4d9417ca09959f4c98aca3dda9226e1f060e249
Cory House <housecor@gmail.com>
2020-10-27 11:42:52 -0500

    convert to ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 // 1. TypeScript
+// 5. Write automated tests
+// 10. Hooks - useEffect
 // 2. Implement login and load user data
 // 3. Implement a 2nd page and allow navigating between the two
 // 4. Display confirmation page with "finished form" (not a pdf)
-// 5. Write automated tests
 // 6. Validate onBlur/onChange - Can use "discover" pattern for onBlur.
 // 7. async/await
 // 8. Error handling
 // 9. React-query / swr
-// 10. Hooks
 // 11. Keys
 // 12. proptypes
 // 13. a11y
+// 14. Follow-up fields / conditional rendering
 
 import React, { useState } from "react";
 import "./App.css";
@@ -20,28 +21,29 @@ import Container from "react-bootstrap/Container";
 import Page1 from "./Page1";
 import Page2 from "./Page2";
 import { saveInsured } from "./api/insured";
+import { Insured } from "./typings/types";
 
 const emptyInsured = {
   name: "",
   dob: "",
   isSmoker: false,
-  weight: null,
+  weight: undefined,
 };
 
 export default function App() {
-  const [insured, setInsured] = useState(emptyInsured); // Hey react, when this data changes, redraw the screen for me.
+  const [insured, setInsured] = useState<Insured>(emptyInsured); // Hey react, when this data changes, redraw the screen for me.
   const [errors, setErrors] = useState({});
   const [currentPage, setCurrentPage] = useState(1);
 
-  function onChange(event) {
+  function onChange(event: React.ChangeEvent<HTMLInputElement>) {
     setInsured({
       ...insured, // Copy state using the spread syntax. Must copy state since state is immutable.
       [event.target.id]: event.target.value, // Using a computed property to reference a property using a variable
     });
   }
 
-  function getErrors(id) {
-    const _errors = id ? { ...errors } : {}; // using underscore prefix to avoid naming collision with errors in state.
+  function getErrors(id?: string) {
+    const _errors: any = id ? { ...errors } : {}; // using underscore prefix to avoid naming collision with errors in state.
     if (id) delete _errors[id];
 
     if (currentPage === 1) {
@@ -63,11 +65,11 @@ export default function App() {
     return _errors;
   }
 
-  function validate(event) {
+  function validate(event: React.FocusEvent) {
     setErrors(getErrors(event.target.id));
   }
 
-  function onSubmit(event) {
+  function onSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault(); // Tell the browser not to reload.
 
     // If errors exist, stop submit.
@@ -85,10 +87,11 @@ export default function App() {
 
   function renderPage() {
     const props = {
-      insured: insured,
-      onChange: onChange,
-      validate: validate,
-      errors: errors,
+      // Object shorthand
+      insured,
+      onChange,
+      validate,
+      errors,
     };
 
     if (currentPage === 1) return <Page1 {...props} />;
