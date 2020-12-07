e1b4581daf6da494d8e2dabc14fe3e73a9572d15
Cory House <housecor@gmail.com>
2020-12-07 14:51:01 -0600

    Describe pure func benefits

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -1,3 +1,10 @@
+/* Pure function benefits:
+1. Easy to test.
+2. No side-effects
+3. Idempotent (always returns the same thing for the same args).
+4. Depends on no outside state. only uses the args passed in.
+5. Composable.
+*/
 export function add(x: number, y: number) {
   return x + y;
 }
